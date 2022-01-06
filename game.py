4a54f8bf9909711fa424df63121853cf19be545d
Aymeric-B <aymeric.boffa@gmail.com>
2022-01-06 09:35:47 +0100

    Game dev with TDD

---
+++ game.py
@@ -0,0 +1,25 @@
+# game.py
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self,word):
+        if not word:
+            return False
+
+        temp_grid = self.grid.copy()
+        for letter in word:
+            if letter in temp_grid:
+                temp_grid.remove(letter)
+            else:
+                return False
+
+        return True
